c186d7e7a7478a992789449c6628c89f0d26ad06
Julian Bieber <j.bieber@patagona.de>
2019-08-03 17:19:43 +0200

    -

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[macro_use]
+extern crate structopt;
+use structopt::StructOpt;
+
 use std::net::{TcpListener, TcpStream};
 use std::io::{Write, Read};
 use std::time::Duration;
@@ -10,15 +14,38 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<usize> {
     Ok(0)
 }
 
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(name = "tcp-client")]
+struct Options {
+    #[structopt(short, long)]
+    threads: i64,
+    #[structopt(short, long)]
+    address: String,
+    #[structopt(short, long)]
+    new_connections: bool
+}
+
+
 fn main() {
-    for _ in 0..10 {
-        std::thread::spawn(|| {
-            let stream = TcpStream::connect("127.0.0.1:1111").unwrap();
-            let data = "1".repeat(1024);
-            handle_client(stream).unwrap()
-        });
-    }
-    loop{
-        std::thread::sleep(Duration::from_secs(5));
+    let options: Options = Options::from_args();
+    if options.new_connections {
+        loop {
+            let mut stream = TcpStream::connect(options.address.as_str()).unwrap();
+            let mut data: [u8; 1024] = [0; 1024];
+            for _ in 0..1024{
+                stream.read(&mut data).unwrap();
+            };
+        }
+    } else {
+        for _ in 0..options.threads {
+            let o = options.clone();
+            std::thread::spawn(move || {
+                let stream = TcpStream::connect(o.address.as_str()).unwrap();
+                handle_client(stream).unwrap()
+            });
+        }
+        loop{
+            std::thread::sleep(Duration::from_secs(5));
+        }
     }
 }
